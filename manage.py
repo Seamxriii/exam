9537351752fcebec674e72c25098d453bf209ece
Seamxriii <niki250812003@gmail.com>
2026-06-20 07:55:03 +0300

    Add files via upload

--- manage.py
+++ manage.py
@@ -18,3 +18,4 @@ if __name__ == "__main__":
 # uv run python manage.py migrate
 # uv run python manage.py createsuperuser
 # uv run python manage.py runserver
+# git clone https://github.com/Seamxriii/exam.git
